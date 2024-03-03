66b9cfbe264c2c9f256e4276d2560f8cb55f7f35
임현민 <120604k@gmail.com>
2024-03-03 17:41:37 +0900

    readme.md, doProcess

---
+++ workspace/day12/src/main/java/com/codingbox/web/servlet/DoFrontcontroller.java
@@ -0,0 +1,26 @@
+package com.codingbox.web.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class DoFrontcontroller extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+
+	}
+	protected void doProcess (HttpServletRequest req, HttpServletResponse resp) {
+		System.out.println("URI: " + req.getRequestURI());
+	}
+}
